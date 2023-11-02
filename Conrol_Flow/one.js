745acbaaaa07dc8b075350c35c2d2657cfc6c7ee
vishalkumarsingh <109060578+vishalkumarsingh015@users.noreply.github.com>
2023-11-02 20:25:04 +0000

    control flow code file

---
+++ Conrol_Flow/one.js
@@ -0,0 +1,41 @@
+const temperature=40;
+if (temperature==40){
+   // console.log("less than 50")
+}else{
+   // console.log("temperature is greater than 50")
+}
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+ 
+
+const score=200;
+if (score>100){
+    let power="fly"
+    //console.log(`user power: ${power}`)
+}
+//console.log(`user power: ${power}`) //not defin
+//if else
+const balance=1000
+if(balance<500){
+    //console.log("less than 500");
+}else if
+(balance <700){
+   // console.log("less than 700");
+}else if(balance<900){
+    console.log("less than 900");
+}else{
+  //  console.log("less than 1200");
+}
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
