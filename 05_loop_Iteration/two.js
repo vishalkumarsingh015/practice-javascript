1b43fbbba9ce52cadbb34a40a6c54a18bcaf78c1
vishalkumarsingh <109060578+vishalkumarsingh015@users.noreply.github.com>
2023-11-03 19:47:31 +0000

    dowhileloop

---
+++ 05_loop_Iteration/two.js
@@ -0,0 +1,22 @@
+let index=0
+while (index<=10){
+   // console.log(`value of index is : ${index}`);
+    index=index+2;
+
+}
+//Array
+ 
+let MyArray=["vishal","ankit","anuj"]
+let arr=0
+while(arr<MyArray.length){
+   // console.log(`Valu is ${MyArray[arr]}`);
+    arr=arr+1
+}
+
+//do while loop
+
+let scor= 11
+do{
+    console.log(`score id ${scor}`);
+    scor++
+}while(scor<=10);
